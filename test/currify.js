a42bf0d52664b4c2c63cec8a6fe4adb19d4afd89
coderaiser <coderaiser@cloudcmd.io>
2016-11-23 14:37:19 +0200

    test(currify) function.length: 3

--- test/currify.js
+++ test/currify.js
@@ -24,7 +24,7 @@ test('call returned function', t => {
     t.end();
 });
 
-test('function.length: 3', t => {
+test('function.length: 2', t => {
     const fn = currify((a, b, c) => {
         return a + b + c;
     });
@@ -44,20 +44,40 @@ test('function.length: 3', t => {
     t.end();
 });
 
-test('function.length: 4', t => {
+test('function.length: 3', t => {
     const fn = currify((a, b, c, d) => {
         return a + b + c + d;
     });
     
     const sum = (f) => {
-        if (f.length === 2)
-            return f(2, 3)
+        if (f.length === 3)
+            return f(2, 3, 0)
         
         if (f.length === 1)
             return f();
     };
     
-    const result = sum(fn(1, 0));
+    const result = sum(fn(1));
+    
+    t.equal(result, 6, 'shold return result');
+    
+    t.end();
+});
+
+test('function.length: 4', t => {
+    const fn = currify((a, b, c, d, e) => {
+        return a + b + c + d + e;
+    });
+    
+    const sum = (f) => {
+        if (f.length === 4)
+            return f(2, 3, 0, 0 )
+        
+        if (f.length === 1)
+            return f();
+    };
+    
+    const result = sum(fn(1));
     
     t.equal(result, 6, 'shold return result');
     

050f34f2ef59fb26134c3d08a2be43601b221106
coderaiser <coderaiser@cloudcmd.io>
2017-05-23 17:27:43 +0300

    chore(package) v2.0.4

--- dist/currify.js
+++ dist/currify.js
@@ -1,11 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.currify = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"currify":[function(require,module,exports){
 'use strict';
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-var tail = function tail(list) {
-    return [].slice.call(list, 1);
-};
 var f = function f(fn) {
     return [function (a) {
         return fn.apply(undefined, arguments);
@@ -21,17 +16,19 @@ var f = function f(fn) {
 };
 
 module.exports = function currify(fn) {
+    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+        args[_key - 1] = arguments[_key];
+    }
+
     check(fn);
 
-    var args = tail(arguments);
-
-    if (args.length >= fn.length) return fn.apply(undefined, _toConsumableArray(args));
+    if (args.length >= fn.length) return fn.apply(undefined, args);
 
     var again = function again() {
-        return currify.apply(undefined, [fn].concat(_toConsumableArray(args), Array.prototype.slice.call(arguments)));
+        return currify.apply(undefined, [fn].concat(args, Array.prototype.slice.call(arguments)));
     };
 
-    var count = fn.length - arguments.length;
+    var count = fn.length - args.length - 1;
     var func = f(again)[count];
 
     return func || again;
